e2db68551dff5cdbb6c0aa0e3a95c73f72a0f07d
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-01-18 19:00:05 +0100

    Route implementation customised for mongodb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import db from "./database.js";
+import { ObjectId } from "mongodb";
 
 // ========== Setup ========== //
 
@@ -22,31 +23,40 @@ server.get("/", (req, res) => {
 
 // Get all contacts (GET /contacts)
 server.get("/contacts", async (req, res) => {
-    const query = "SELECT * FROM contacts"; // SQL query
-    const [contacts] = await db.execute(query); // Execute the query
-
+    const contacts = await db
+        .collection("contacts")
+        .find()
+        .sort({ first: 1, last: 1 })
+        .toArray(); // Get all contacts from database
     res.json(contacts); // Send the results as JSON
 });
 
 // Search contacts (GET /contacts/search?q=)
 server.get("/contacts/search", async (req, res) => {
     const searchString = req.query.q.toLowerCase(); // get query string from request URL and lowercase it
-    const query = "SELECT * FROM contacts WHERE LOWER(first) LIKE ? OR LOWER(last) LIKE ?"; // SQL query
-    const values = [`%${searchString}%`, `%${searchString}%`]; // values to search for
-    const [results] = await db.execute(query, values); // Execute the query
+    const query = {
+        $or: [
+            { first: { $regex: searchString, $options: "i" } },
+            { last: { $regex: searchString, $options: "i" } }
+        ]
+    }; // MongoDB query
+
+    const results = await db
+        .collection("contacts")
+        .find(query)
+        .sort({ first: 1, last: 1 })
+        .toArray(); // Execute the query
 
     res.json(results); // Send the results as JSON
 });
 
 // Get single contact (GET /contacts/:id)
 server.get("/contacts/:id", async (req, res) => {
-    const id = Number(req.params.id); // get id from request URL and convert it to a number
-    const query = "SELECT * FROM contacts WHERE id = ?"; // SQL query
-    const values = [id]; // values to search for (id)
-    const [contacts] = await db.execute(query, values); // Execute the query
+    const id = req.params.id; // get id from request URL
+    const contact = await db.collection("contacts").findOne({ _id: new ObjectId(id) }); // Get contact from database
 
-    if (contacts.length > 0) {
-        res.json(contacts[0]); // return first contact from results as JSON
+    if (contact) {
+        res.json(contact); // return first contact from results as JSON
     } else {
         res.status(404).json({ message: "Contact not found!" }); // otherwise return 404 and error message
     }
@@ -55,12 +65,13 @@ server.get("/contacts/:id", async (req, res) => {
 // Create contact (POST /contacts)
 server.post("/contacts", async (req, res) => {
     const newContact = req.body; // get new contact object from request body
-    const query = "INSERT INTO contacts (first, last, twitter, avatar) VALUES (?, ?, ?, ?)"; // SQL query
-    const values = [newContact.first, newContact.last, newContact.twitter, newContact.avatar]; // values to insert
-    const [result] = await db.execute(query, values); // Execute the query
 
-    if (result.affectedRows > 0) {
-        res.json({ message: "Created new contact", id: result.insertId }); // return message and id of new contact
+    const result = await db.collection("contacts").insertOne(newContact); // Insert new contact into database
+
+    console.log(result);
+
+    if (result.acknowledged) {
+        res.json({ message: "Created new contact", id: result.insertedId }); // return message and id of new contact
     } else {
         res.status(500).json({ message: "Failed to create new contact" }); // return error message
     }
@@ -68,44 +79,51 @@ server.post("/contacts", async (req, res) => {
 
 // Update contact (PUT /contacts/:id)
 server.put("/contacts/:id", async (req, res) => {
-    const id = Number(req.params.id); // get id from request URL
+    const id = req.params.id; // get id from request URL
     const updatedContact = req.body; // get updated properties from request body
-    const query = "UPDATE contacts SET first = ?, last = ?, twitter = ?, avatar = ? WHERE id = ?"; // SQL query
-    const values = [updatedContact.first, updatedContact.last, updatedContact.twitter, updatedContact.avatar, id]; // values to update
-    const [result] = await db.execute(query, values); // Execute the query
+    const result = await db
+        .collection("contacts")
+        .updateOne({ _id: new ObjectId(id) }, { $set: updatedContact }); // Update contact in database
 
-    if (result.affectedRows === 0) {
-        res.status(404).json({ message: "Contact not found!" }); // return 404 if contact was not found
+    if (result.acknowledged) {
+        res.json({ message: `Updated contact with id ${id}` }); // return message
     } else {
-        res.json({ message: `Updated contact with id ${id}` }); // return result from database
+        res.status(500).json({ message: "Failed to update contact" }); // return error message
     }
 });
 
 // Delete contact (DELETE /contacts/:id)
 server.delete("/contacts/:id", async (req, res) => {
-    const id = Number(req.params.id); // get id from request URL
-    const query = "DELETE FROM contacts WHERE id = ?"; // SQL query
-    const values = [id]; // values to delete
-    const [result] = await db.execute(query, values); // Execute the query
+    const id = req.params.id; // get id from request URL
 
-    if (result.affectedRows === 0) {
-        res.status(404).json({ message: "Contact not found!" }); // return 404 if contact was not found
-    } else {
+    const result = await db.collection("contacts").deleteOne({ _id: new ObjectId(id) }); // Delete contact from database
+
+    if (result.acknowledged) {
         res.json({ message: `Deleted contact with id ${id}` }); // return message
+    } else {
+        res.status(500).json({ message: "Failed to delete contact" }); // return error message
     }
 });
 
 // Toggle favorite property of contact (PUT /contacts/:id/favorite)
 server.put("/contacts/:id/favorite", async (req, res) => {
-    const id = Number(req.params.id); // get id from request URL
-    const query = "UPDATE contacts SET favorite = !favorite WHERE id = ?"; // SQL query
-    const values = [id]; // values to update
-    const [result] = await db.execute(query, values); // Execute the query
+    const id = req.params.id; // get id from request URL
 
-    if (result.affectedRows === 0) {
-        res.status(404).json({ message: "Contact not found!" }); // return 404 if contact was not found
-    } else {
+    const contact = await db.collection("contacts").findOne({ _id: new ObjectId(id) }); // Get the contact from the database
+
+    if (contact) {
+        const newFavoriteValue = !contact.favorite; // Toggle the favorite field
+        // Update the contact in the database
+        const result = await db
+            .collection("contacts")
+            .updateOne(
+                { _id: new ObjectId(id) },
+                { $set: { favorite: newFavoriteValue } }
+            );
+
         res.json({ message: `Toggled favorite property of contact with id ${id}` }); // return message
+    } else {
+        res.status(404).json({ message: "Contact not found!" }); // return 404 if contact was not found
     }
 });
 
