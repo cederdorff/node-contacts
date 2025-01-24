6b5617b91dfcefe2508faedf1a1598e4dba7d5b7
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-01-24 07:20:04 +0100

    Enhance comments for clarity in server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Imports
+// ========== Imports ========== //
 import express from "express";
 import cors from "cors";
 import db from "./database.js";
@@ -126,7 +126,7 @@ server.patch("/contacts/:id/favorite", async (req, res) => {
 
 // ========== Start server ========== //
 
-// Start server on port 3000
+// Start server on port (3000)
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
